638e58bdb6fd2f1189caa0b11beaa7779c9dc20a
speGit-ai <turnzz@163.com>
2021-02-19 13:19:24 +0800

    py

--- telegram_download.py
+++ telegram_download.py
@@ -18,8 +18,8 @@ channel_link 要下载图片的频道链接
 proxy 将localhost改成代理地址,12345改成代理端口
 picture_storage_path 图片下载到的路径
 '''
-api_id = 809268
-api_hash = "4777715f01d208f7e4dfed2f44c3c702"
+api_id = 123456
+api_hash = ""
 
 
 proxy = ("http", "127.0.0.1", 10809) #不需要代理的话删掉该行
